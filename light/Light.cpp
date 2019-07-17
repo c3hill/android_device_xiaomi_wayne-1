ef06b1085a572f95261f855dbf9c0ca2dedb13d4
TH779 <tongsj2333@gmail.com>
2019-07-18 00:41:31 +0800

    wayne: light: Set max LED brightness and max LCD brightness
     * From https://github.com/GuaiYiHu/android_device_xiaomi_whyred
     * Fix brightness adjustment
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- light/Light.cpp
+++ light/Light.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,9 @@ namespace implementation {
 #define RAMP_STEP_MS               "ramp_step_ms"
 #define START_IDX                  "start_idx"
 
+#define MAX_LED_BRIGHTNESS    255
+#define MAX_LCD_BRIGHTNESS    4095
+
 /*
  * 8 duty percent steps.
  */
@@ -71,6 +74,37 @@ static void set(std::string path, int value) {
     set(path, std::to_string(value));
 }
 
+static uint32_t getBrightness(const LightState& state) {
+    uint32_t alpha, red, green, blue;
+
+    /*
+     * Extract brightness from AARRGGBB.
+     */
+    alpha = (state.color >> 24) & 0xFF;
+    red = (state.color >> 16) & 0xFF;
+    green = (state.color >> 8) & 0xFF;
+    blue = state.color & 0xFF;
+
+    /*
+     * Scale RGB brightness if Alpha brightness is not 0xFF.
+     */
+    if (alpha != 0xFF) {
+        red = red * alpha / 0xFF;
+        green = green * alpha / 0xFF;
+        blue = blue * alpha / 0xFF;
+    }
+
+    return (77 * red + 150 * green + 29 * blue) >> 8;
+}
+
+static inline uint32_t scaleBrightness(uint32_t brightness, uint32_t maxBrightness) {
+    return brightness * maxBrightness / 0xFF;
+}
+
+static inline uint32_t getScaledBrightness(const LightState& state, uint32_t maxBrightness) {
+    return scaleBrightness(getBrightness(state), maxBrightness);
+}
+
 /*
  * Scale each value of the brightness ramp according to the
  * brightness of the color.
@@ -87,28 +121,14 @@ static std::string getScaledRamp(uint32_t brightness) {
 }
 
 static void handleWayneBacklight(Type /*type*/, const LightState& state) {
-    uint32_t brightness = state.color & 0xFF;
+    uint32_t brightness = getScaledBrightness(state, MAX_LCD_BRIGHTNESS);
     set(LCD_LED BRIGHTNESS, brightness);
 }
 
 static void setNotification(const LightState& state) {
-    uint32_t redBrightness, brightness;
+    uint32_t redBrightness = getScaledBrightness(state, MAX_LED_BRIGHTNESS);
 
-    /*
-     * Extract brightness from RGB.
-     */
-    redBrightness   = (state.color >> 16) & 0xFF;
-    brightness      = (state.color >> 24) & 0xFF;
-
-    /*
-     * Scale RGB brightness if the Alpha brightness is not 0xFF.
-     */
-    if (brightness != 0xFF) {
-        redBrightness   = (redBrightness * brightness) / 0xFF;
-    }
-
-    /* Turn off the leds (initially) */
-    set(RED BRIGHTNESS, 0);
+    /* Disable blinking */
     set(RED BLINK, 0);
 
     if (state.flashMode == Flash::TIMED) {
